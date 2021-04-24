8ffdb8c279f819582aebc6f6c4b43c40051aec4f
rizkyiqbal <rizkiiqbal36@gmail.com>
2021-04-24 14:51:34 +0700

    ci: 🎡 fix job and stage name

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,8 +2,7 @@ import mongoose from 'mongoose'
 import config from '@root/app/config/appConfig'
 
 const dbName = process.env.APP_ENV === 'local' ? `${config.mongodb.db}_test` : config.mongodb.db
-console.log('env => ', process.env.APP_ENV)
-console.log(config.mongodb.uri)
+
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
