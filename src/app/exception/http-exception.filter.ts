373ff0b60e8e0884d68678d82ae395cc6ec3dcff
rizkyiqbal <rizkiiqbal36@gmail.com>
2021-04-26 09:43:53 +0700

    feat: 🎸 implement joi error message

--- src/app/exception/http-exception.filter.ts
+++ src/app/exception/http-exception.filter.ts
@@ -12,15 +12,20 @@ export class HttpExceptionFilter implements ExceptionFilter {
     let message = 'INTERNAL_SERVER_ERROR'
 
     let result: any
+    let detail: any
     if (exception instanceof HttpException) {
       status = exception.getStatus()
       result = exception.getResponse() as IExceptionResponse
     }
+    if (result?.options?.joiError) detail = result?.options?.joiError?.details
+    console.log(result?.options?.joiError)
+
     if (status !== 500) message = result.flag
     response.status(status).json({
       statusCode: status,
       path: request.url,
       message,
+      detail,
       timestamp: new Date().toISOString()
     })
   }
